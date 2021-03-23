c6deea71dd437eae350047404a8fe2fd11b9c806
Mustafa UYSAL <18701031@mersin.edu.tr>
2021-03-23 23:44:59 +0300

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
+import 'package:personal_library/views/home.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
     );
   }
 }
